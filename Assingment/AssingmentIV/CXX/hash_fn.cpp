22ff02dc455416bee0b1fbb3efb5f51abf494815
karlluo1k <karlluo1k@gmail.com>
2025-11-24 20:19:50 +0800

    Implement myHashInt by deviding by a prime number

--- Assingment/AssingmentIV/CXX/hash_fn.cpp
+++ Assingment/AssingmentIV/CXX/hash_fn.cpp
@@ -11,12 +11,18 @@
     - 2025/11/17: Refactored to use hash_fn.hpp
 
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+
+
+   Development History:
+    - 2025/11/22: Initial commit: Add template files
+    - 2025/11/24: Implement myHashInt
+
+   Developer: Yi-Kai Lo <s1133304@mail.yzu.edu.tw>
  */
 #include "hash_fn.hpp"
 
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
-    return key % m;  // basic division method
+    return key % 97;
 }
 
 int myHashString(const std::string& str, int m) {
